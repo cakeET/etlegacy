c28bcd0f6be3fe1d508514e137d24329fd600943
cake <>
2022-06-19 14:43:24 -0400

    extra checks 6 half mod

--- src/client/cl_cgame.c
+++ src/client/cl_cgame.c
@@ -1345,12 +1345,12 @@ void CL_AdjustTimeDelta(void)
 				//calculate threshold for advancing time using modulo
 				if (svFrameTime > cls.frametime)
 				{
-					threshold -= (svFrameTime % cls.frametime);
+					threshold -= (svFrameTime % cls.frametime) / 2;
 					if (cl_showTimeDelta->integer & 1) Com_Printf("MODNORM ");
 				}
 				else
 				{
-					threshold -= (cls.frametime % svFrameTime);
+					threshold -= (cls.frametime % svFrameTime) / 2;
 					if (cl_showTimeDelta->integer & 1) Com_Printf("MODFLIP ");
 				}
 
